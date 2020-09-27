5999509f18474055a5d8603d69bfafe495a5806e
Natalia <wasiluk.natalia@gmail.com>
2020-09-27 16:37:15 +0200

    Contact us section ready

--- src/components/FooterContact.js
+++ src/components/FooterContact.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import 'font-awesome/css/font-awesome.min.css';
 
-const FooterSectionContainer = styled.div`
+const FooterContact = styled.div`
     padding-bottom: 30px;
     border-bottom: 1px solid ${props => props.theme.lightGrey};
     margin-bottom: 20px;
@@ -18,14 +19,50 @@ const FooterSectionContainer = styled.div`
     @media (min-width: 980px){
         flex-basis: 24%;
     }
+    .icon-container {
+        display: inline-flex;
+        margin-bottom: 1.4em;
+    }
+    i { 
+        width: 40px;
+        height: 40px;
+        background-color: ${props => props.theme.grey};
+        border-radius: 6px;
+        color: white;
+        font-size: 1.8em;
+        margin-right: 0.2em;
+        display: inline-flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        transition: .2s;
+    }
+    i:hover{
+        background-color: ${props => props.theme.black}
+    }
+    p { 
+        font-family: 'Open Sans Light';
+        font-size: 1em;
+        line-height: 2em;
+        margin-top: 0.6em;
+        color: ${props => props.theme.black};
+    }
 `;
 
 const FooterSection = ({ title, linksArray = [] }) => {
     return (
-        <FooterSectionContainer>
+        <FooterContact>
             <h2>Contact Us</h2>
-            <p> place for icons</p>
-        </FooterSectionContainer>
+            <div className='icon-contaier'>
+                <i class="fa fa-twitter"></i>
+                <i class="fa fa-facebook"></i>
+                <i class="fa fa-instagram"></i>
+                <i class="fa fa-pinterest"></i>
+            </div>
+            <p>1234 Fictional Road</p>
+            <p>Nashville, TN 00000</p>
+            <p>(800) 555-0000</p>
+        </FooterContact>
     )
 }
 
